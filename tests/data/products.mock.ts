92a8d80c91938b0c85a7d4fe9ed52dfd872abfbc
Thitiporn2045 <142084816+Thitiporn2045@users.noreply.github.com>
2025-11-30 21:38:20 +0700

    feat: add products mock

---
+++ tests/data/products.mock.ts
@@ -0,0 +1,71 @@
+export interface ProductMock {
+    title: string;
+    sku: string;
+    description: string;
+    price: string;
+}
+
+export const productsMock: ProductMock[] = [
+    {
+        title: "TerraFlex Hoodie",
+        sku: "0000000001",
+        description: "Men's hoodie for gym. Crafted from recycled polyester",
+        price: "79.69",
+    },
+    {
+        title: "NordicPeak Jacket",
+        sku: "0000000002",
+        description: "Women's jacket for casual. Crafted from GORE-TEX",
+        price: "206.20",
+    },
+    {
+        title: "PolarStep Yoga Tights",
+        sku: "0000000003",
+        description: "Unisex tights for yoga. Crafted from recycled polyester",
+        price: "48.60",
+    },
+    {
+        title: "PolarStep Hiking Sports Bra",
+        sku: "0000000004",
+        description:
+            "Women's sports bra for hiking. Crafted from nylon-spandex",
+        price: "55.14",
+    },
+    {
+        title: "Kido Running Jacket - Black",
+        sku: "0000000005",
+        description:
+            "Unisex jacket for running. Crafted from recycled polyester",
+        price: "192.93",
+    },
+    {
+        title: "Astra Tights - Black",
+        sku: "0000000006",
+        description: "Unisex tights for training. Crafted from organic cotton",
+        price: "66.65",
+    },
+    {
+        title: "Astra Hiking Cap",
+        sku: "0000000007",
+        description: "Unisex cap for hiking. Crafted from recycled polyester",
+        price: "26.27",
+    },
+    {
+        title: "Astra Outdoor Beanie - Royal Blue",
+        sku: "0000000008",
+        description: "Women's beanie for outdoor. Crafted from GORE-TEX",
+        price: "33.95",
+    },
+    {
+        title: "TerraFlex Running Socks - Navy",
+        sku: "0000000009",
+        description: "Men's socks for running. Crafted from nylon-spandex",
+        price: "17.11",
+    },
+    {
+        title: "TerraFlex Gloves",
+        sku: "0000000010",
+        description: "Women's gloves for outdoor. Crafted from poly-cotton",
+        price: "22.24",
+    },
+];
