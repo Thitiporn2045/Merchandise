46ed5aa86aaa982dad485bdd704b0de55b377e1d
Thitiporn2045 <142084816+Thitiporn2045@users.noreply.github.com>
2025-11-30 22:24:54 +0700

    fix: delete headless in playwright.config page

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,6 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://merchandise-dev.odds.team/',
-    headless: false,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
